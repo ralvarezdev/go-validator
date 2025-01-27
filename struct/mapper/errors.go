101c4f7654998b0cb07e2ca9a488c4763d5c84e6
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-26 21:46:31 -0400

    minor: added ErrNilGenerator error

--- struct/mapper/errors.go
+++ struct/mapper/errors.go
@@ -1,6 +1,11 @@
 package mapper
 
+import (
+	"errors"
+)
+
 var (
+	ErrNilGenerator            = errors.New("generator cannot be nil")
 	ErrProtobufTagNotFound     = "missing protobuf tag: %s"
 	ErrProtobufTagNameNotFound = "missing protobuf tag name: %s"
 	ErrEmptyJSONTag            = "empty json tag: %s"
